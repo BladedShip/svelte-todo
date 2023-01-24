4c2fe83d3476fb2963debffe46ce1ce59bf000c8
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-01-24 12:50:48 +0530

    Update firebaseConfig.js

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,19 +1,11 @@
-// import { API_KEY,AUTH_DOMAIN,PROJECT_ID,STORAGE_BUCKET,MESSAGING_SENDER_ID,APP_ID } from '$env/static/private';
-
-// export const firebaseConfig = {
-//     apiKey: import.meta.env.API_KEY,
-//     authDomain: import.meta.env.AUTH_DOMAIN,
-//     projectId: import.meta.env.PROJECT_ID,
-//     storageBucket: import.meta.env.STORAGE_BUCKET,
-//     messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-//     appId: import.meta.env.APP_ID
-//   };
+import { API_KEY,AUTH_DOMAIN,PROJECT_ID,STORAGE_BUCKET,MESSAGING_SENDER_ID,APP_ID } from '$env/static/private';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyD4aTimqRgvL1Mzse9RkSAQ_EcfdxnwtYU",
-    authDomain: "sveltekit-17026.firebaseapp.com",
-    projectId: "sveltekit-17026",
-    storageBucket: "sveltekit-17026.appspot.com",
-    messagingSenderId: "585826008166",
-    appId: "1:585826008166:web:fecc5112120f0220a001a7"
+    apiKey: import.meta.env.API_KEY,
+    authDomain: import.meta.env.AUTH_DOMAIN,
+    projectId: import.meta.env.PROJECT_ID,
+    storageBucket: import.meta.env.STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+    appId: import.meta.env.APP_ID
   };
+
